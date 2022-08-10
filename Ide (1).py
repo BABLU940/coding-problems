32e5e7ad55ab10f472e01c394dc63f7856599762
BABLU940 <65236839+BABLU940@users.noreply.github.com>
2022-08-11 00:57:23 +0530

    shoping cost

---
+++ Ide (1).py
@@ -0,0 +1,10 @@
+# shoping cost (list) problem....
+n=int(input())
+while(n!=0):
+  x=input().split()
+  p=int(x[0])
+  q=int(x[1])
+  if(p>100):
+    print(float((p*q)-(p*q)/5))
+  print(float(p*q))  
+  n=n-1  
