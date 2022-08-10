36d29740acd3894e95a8a9e7468cc1ec2c343d04
BABLU940 <65236839+BABLU940@users.noreply.github.com>
2022-08-11 01:00:31 +0530

    Update Ide (1).py

--- Ide (1).py
+++ Ide (1).py
@@ -8,3 +8,14 @@ while(n!=0):
     print(float((p*q)-(p*q)/5))
   print(float(p*q))  
   n=n-1  
+#shoping cost 
+n=int(input())
+while(n!=0):
+  x=input().split()
+  p=int(x[0])
+  q=int(x[1])
+  if(p>100):
+    print(float((p*q)-(p*q)/5))
+  else:  
+    print(float(p*q))  
+  n=n-1  
